7a906b9f3f914f68b52de550a460c102d22b908c
jay-hankins <jay.hankins@outlook.com>
2014-11-26 11:30:01 -0500

    Updating FoxitReader package again

--- FoxitReader7/tools/chocolateyInstall.ps1
+++ FoxitReader7/tools/chocolateyInstall.ps1
@@ -1,2 +1,4 @@
-$package = 'FoxitReader7'
-Install-ChocolateyPackage 'FoxitReader703.0916_enu_Setup.exe' 'EXE' '/verysilent' 'http://cdn01.foxitsoftware.com/pub/foxit/reader/desktop/win/7.x/7.0/en_us/FoxitReader703.0916_enu_Setup.exe'
+﻿$name = 'FoxitReader7'
+$url  = 'http://cdn01.foxitsoftware.com/pub/foxit/reader/desktop/win/7.x/7.0/en_us/FoxitReader703.0916_enu_Setup.exe'
+
+Install-ChocolateyPackage $name 'EXE' '/VERYSILENT' $url
